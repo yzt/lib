082dc5bbb1fdc9c1cc233e833cea7ed22422c2ba
yzt <yaserzt@gmail.com>
2018-06-13 02:58:41 +0430

    [ALLOCATOR] Ring stuff.
    These are just micro edits, since I don't have time to sit and write this code. Sorry for the commit spam.

--- ylib/y_ring_allocator.cpp
+++ ylib/y_ring_allocator.cpp
@@ -34,4 +34,80 @@ bool RingAllocator::free (Buffer buffer) {
 }
 #endif
 
+#include "y_ring_allocator.hpp"
+#include <cassert>
 
+struct Header {
+    allocator_size_t size_and_occupied;
+};
+
+typedef Header Footer;
+
+static_assert(sizeof(Header) == 4, "");
+static_assert(sizeof(Footer) == 4, "");
+
+static inline void
+PutHeaderAt (uint8_t * ptr, allocator_size_t user_size, bool occupied) {
+    assert((user_size & 1) == 0);
+    auto p = reinterpret_cast<Header *>(ptr);
+    p->size_and_occupied = user_size | (occupied ? 1 : 0);
+}
+
+static inline uint32_t
+GetSizeFromHeader (uint8_t const * ptr) {
+    auto p = reinterpret_cast<Header const *>(ptr);
+    return p->size_and_occupied & ~allocator_size_t(1);
+}
+
+static inline uint32_t
+GetOccupiedFromHeader (uint8_t const * ptr) {
+    auto p = reinterpret_cast<Header const *>(ptr);
+    return 0 != (p->size_and_occupied & 1);
+}
+
+static inline void
+PutFooterAt (uint8_t * ptr, allocator_size_t user_size, bool occupied) {
+    return PutHeaderAt(ptr, user_size, occupied);
+}
+
+static inline uint32_t
+GetSizeFromFooter (uint8_t const * ptr) {
+    return GetSizeFromHeader(ptr);
+}
+
+static inline uint32_t
+GetOccupiedFromFooter (uint8_t const * ptr) {
+    return GetOccupiedFromHeader(ptr);
+}
+
+static inline void
+PutBlockAt (uint8_t * ptr, uint32_t total_size) {
+    assert(total_size >= sizeof(Header) + sizeof(Footer));
+    ...
+}
+
+
+bool
+Allocator_Ring_Init (
+    allocator_ring_t * out_allocator,
+    allocator_block_t backing_memory
+) {
+}
+
+bool
+Allocator_Ring_Cleanup (
+    allocator_ring_t * allocator
+) {
+}
+
+allocator_block_t
+Allocator_Ring_Alloc (
+    allocator_size_t size
+) {
+}
+
+bool
+Allocator_Ring_Free (
+    allocator_block_t mem
+) {
+}
