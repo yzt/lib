cfdd01559a21fa498f1cae75c8fdf26e9f565b91
yzt <yaserzt@gmail.com>
2019-01-28 16:14:39 +0330

    More work on the profiler. Win32/MSVC only.

--- ylib/y_profiler.cpp
+++ ylib/y_profiler.cpp
@@ -1,17 +1,130 @@
 
 #include "y_profiler.h"
+#include <atomic>
+#include <chrono>
+#include <cstdlib>
+#include <thread>
+using namespace std::chrono_literals;
 
-#include <cstdint>  // uintptr_t
+//#if defined(_WIN32)
+//
+//#if !defined(_WIN32_WINNT)
+//    #define _WIN32_WINNT    0x0501  // We need at least Windows XP for ConvertFiberToThread()...
+//#endif
+//
+//#if !defined(_WIN32_WINNT) || (_WIN32_WINNT < 0x0501)
+//    #error "You must define _WIN32_WINNT to something newer than Win 2000!"
+//#endif
 
+profiler_system_t g_profiler_sys = {};
+static std::thread * g_thread = nullptr;
+static std::atomic_bool g_should_quit = false;
+
+static unsigned const SampleBufferThreshold = 1'048'576;
+static unsigned const SampleBufferSize = 4 * SampleBufferThreshold;
+static profiler_sample_t sample_buffer_1 [SampleBufferSize];
+static profiler_sample_t sample_buffer_2 [SampleBufferSize];
+
+static inline uint64_t
+Profiler_Internal_RDTSC () {
 #if defined(_WIN32)
-
-#if !defined(_WIN32_WINNT)
-    #define _WIN32_WINNT    0x0501  // We need at least Windows XP for ConvertFiberToThread()...
+    return __rdtsc();
+#else
+    #error "RDTSC and return it..."
 #endif
+}
 
-#if !defined(_WIN32_WINNT) || (_WIN32_WINNT < 0x0501)
-    #error "You must define _WIN32_WINNT to something newer than Win 2000!"
+static inline void *
+Profiler_Internal_AtomicExchangePointer (void ** ptr, void * new_value) {
+#if defined(_WIN32)
+    #if defined(_WIN64)
+        static_assert(sizeof(void *) == sizeof(int64_t));
+        return (void *)_InterlockedExchange64((int64_t *)ptr, (int64_t)new_value);
+    #else
+        static_assert(sizeof(void *) == sizeof(int32_t));
+        return (void *)_InterlockedExchange((int32_t *)ptr, (int32_t)new_value);
+    #endif
+#else
+    #error "AtomicExhchange, and return the old value in *ptr"
 #endif
+}
 
+static void _cdecl
+Profiler_Internal_Quit (
+) {
+    if (g_thread) {
+        g_should_quit = true;
+        g_thread->join();
+        delete g_thread;
+        g_thread = nullptr;
 
-#endif  // !defined(_WIN32)
+        g_should_quit = false;
+        g_profiler_sys = {};
+    }
+}
+
+static void
+Profiler_Internal_ProcessSamples (
+    profiler_sample_t * sample_buffer,
+    size_t sample_count
+) {
+}
+
+static void
+Profiler_Internal_ThreadFunc (
+) {
+    while (!g_should_quit) {
+        std::this_thread::sleep_for(100ms);
+
+        auto p = g_profiler_sys.next_sample;    // should be atomic...
+        auto current_buffer = g_profiler_sys.current_buffer;
+        auto threshold = g_profiler_sys.buffer_threshold;
+        if (p >= current_buffer + threshold) {
+            profiler_sample_t * next_buffer = nullptr;
+            if (current_buffer == g_profiler_sys.buffer1)
+                next_buffer = g_profiler_sys.buffer2;
+            else
+                next_buffer = g_profiler_sys.buffer1;
+            g_profiler_sys.current_buffer = next_buffer;
+            
+            auto filled_samples_end =
+                (profiler_sample_t *)Profiler_Internal_AtomicExchangePointer(
+                    (void **)&g_profiler_sys.next_sample,
+                    next_buffer
+                );
+
+            Profiler_Internal_ProcessSamples(
+                current_buffer, filled_samples_end - current_buffer
+            );
+        }
+    }
+}
+
+bool
+Profiler_Init (
+) {
+    if (g_profiler_sys.init_count <= 0) {
+        g_profiler_sys.init_count = 1;
+        g_profiler_sys.init_time = Profiler_Internal_RDTSC();
+
+        g_profiler_sys.next_sample = sample_buffer_1;
+        g_profiler_sys.current_buffer = sample_buffer_1;
+        g_profiler_sys.buffer1 = sample_buffer_1;
+        g_profiler_sys.buffer2 = sample_buffer_2;
+
+        g_profiler_sys.buffer_total_size = SampleBufferSize;
+        g_profiler_sys.buffer_threshold = SampleBufferThreshold;
+
+        ::atexit(Profiler_Internal_Quit);
+
+        g_thread = new std::thread (Profiler_Internal_ThreadFunc);
+    }
+
+    return true;
+}
+
+// If you don't init the profiling system, this will ensure that it happens before main()
+// It's OK to call Profiler_Init() multiple times.
+static bool g_first_init_initiator = Profiler_Init();
+
+//#endif  // !defined(_WIN32)
--- ylib/y_profiler.h
+++ ylib/y_profiler.h
@@ -10,36 +10,47 @@
 extern "C" {
 #endif
 
+typedef enum {
+    ProfilerEvent_BlockBegin,
+    ProfilerEvent_BlockEnd,
+} profiler_event_e;
+
 typedef struct {
+    uint32_t event;
+
+    uint32_t thread_id;
     uint64_t timestamp;
     uint32_t core_id;
-    uint32_t thread_id;
+
+    uint32_t line_num;
+    char const * file_name;
+    char const * func_name;
 
     char const * ctx_name;
     char const * user_str;
 
-    char const * func_name;
-    char const * file_name;
-    uint32_t line_num;
-
-    uint64_t code_address;
+    void * called_from;
 } profiler_sample_t;
 
 typedef struct {
-    profiler_sample_t * current_sample;
+    uint64_t total_overhead_cycles;
+    uint64_t total_samples;
+    profiler_sample_t * next_sample;
+
+    profiler_sample_t * current_buffer;
     profiler_sample_t * buffer1;
     profiler_sample_t * buffer2;
-    uint64_t buffer_sample_count;
+    uint64_t buffer_total_size;
+    uint64_t buffer_threshold;
+    uint64_t init_time;
+    int init_count;
 } profiler_system_t;
 
+extern profiler_system_t g_profiler_sys;
 
 
-bool Profiler_SysInit (
-    profiler_system_t * out_sys
-);
-
-bool Profiler_SysCleanup (
-    profiler_system_t * sys
+bool Profiler_Init (
+    
 );
 
 #if defined(_WIN32)
@@ -53,22 +64,41 @@ bool Profiler_SysCleanup (
 static __forceinline void
 Profiler_Internal_CollectSample (
     profiler_system_t * sys,
+    profiler_event_e event,
     char const * literal_context_name,
     char const * literal_user_str,
     char const * literal_function_name,
     char const * literal_file_name,
     int line_number
 ) {
+    uint32_t core_id;
+    uint64_t start_of_sampling = __rdtscp(&core_id);
+
 #if defined(_WIN64)
     profiler_sample_t * sample = (profiler_sample_t *)_InterlockedExchangeAdd64(
-        (int64_t *)&sys->current_sample, sizeof(profiler_sample_t)
+        (int64_t *)&sys->next_sample, sizeof(profiler_sample_t)
     );
-
+    uint32_t thread_id = __readgsdword(0x48);
 #else
     profiler_sample_t * sample = (profiler_sample_t *)_InterlockedExchangeAdd(
         (int32_t *)&sys->current_sample, sizeof(profiler_sample_t)
     );
+    uint32_t thread_id = __readfsdword(0x24);
 #endif
+    sample->event = event;
+    sample->thread_id = thread_id;
+    sample->timestamp = start_of_sampling;
+    sample->core_id = core_id;
+    sample->line_num = line_number;
+    sample->file_name = literal_file_name;
+    sample->func_name = literal_function_name;
+    sample->ctx_name = literal_context_name;
+    sample->user_str = literal_user_str;
+    sample->called_from = _ReturnAddress();
+
+    _InterlockedIncrement64((int64_t *)&sys->total_samples);
+    uint64_t end_of_sampling = __rdtsc();
+    _InterlockedExchangeAdd64((int64_t *)&sys->total_overhead_cycles, end_of_sampling - start_of_sampling);
 }
 #else
 #error "Non-Windows platforms are not supported."
